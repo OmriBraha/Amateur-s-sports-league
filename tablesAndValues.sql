3ea0824412a8dc246eea73ed22a878884f492326
Omri Braha <70472737+OmriBraha@users.noreply.github.com>
2021-12-21 22:01:12 +0200

    Update and rename tablesAndValues to tablesAndValues.sql

--- tablesAndValues.sql
+++ tablesAndValues.sql
@@ -1,4 +1,3 @@
--- MMN 11 -- Omri Braha 205732928
 ---------------------------------------------------------------------
 -- Q1 - creating tables:
 
@@ -68,19 +67,19 @@ insert into team values
 
 -- player --
 insert into player values
-(99, 'omri braha', 'road 1', 0504770117, 1, 4),
-(88, 'noam weiman', 'road 2', 234567890, 6, 2),
-(77, 'tal weiman', 'road 3', 345678901, 4, 3),
-(66, 'yossi bracha', 'road 4', 456789012, 5, 1),
-(44, 'sarit braha', 'road 6', 678901234, 3, 4),
-(33, 'ortal braha', 'road 7', 890123456, 2, 1);
+(99, 'omri b', 'road 1', 0504770117, 1, 4),
+(88, 'noam w', 'road 2', 234567890, 6, 2),
+(77, 'tal w', 'road 3', 345678901, 4, 3),
+(66, 'yossi b', 'road 4', 456789012, 5, 1),
+(44, 'sarit b', 'road 6', 678901234, 3, 4),
+(33, 'ortal b', 'road 7', 890123456, 2, 1);
 
 -- coach --
 insert into coach values
-(23, 'reuven braha', 'road 12', 987654321,0, 4),
-(45, 'hani braha', 'road 13', 876543219,1, 1),
-(67, 'itzik weiman', 'road 14', 765432198,1, 2),
-(89, 'orly weiman', 'road 15', 543421876,0, 3);
+(23, 'reuven b', 'road 12', 987654321,0, 4),
+(45, 'hani b', 'road 13', 876543219,1, 1),
+(67, 'itzik w', 'road 14', 765432198,1, 2),
+(89, 'orly w', 'road 15', 543421876,0, 3);
 
 -- game --
 insert into game values
